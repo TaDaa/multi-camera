f02c56bf1ee18fbfc47b7c309a2dc09cdf0ab676
TaDaa <trevor002@gmail.com>
2019-09-20 15:43:42 -0400

    try another dist approach

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var MultiCamera = require("./multicamera");
+exports.MultiCamera = MultiCamera;
+//# sourceMappingURL=index.js.map
